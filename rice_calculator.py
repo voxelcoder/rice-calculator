d66486b1e50af612477978abac28dc73917d541b
Velican Akcakaya <velican@akcakaya.net>
2020-04-07 20:50:12 +0200

    fixed typo

--- rice_calculator.py
+++ rice_calculator.py
@@ -4,7 +4,7 @@ from tabulate import tabulate
 # from tkinter import *
 
 # This little program takes your input on what type of rice, in which device and how much you want too cook.
-# It then calculates the ammount of water you need! It also shows you neat little infos depending on what
+# It then calculates the amount of water you need! It also shows you neat little infos depending on what
 # variety you cook!
 #
 # Authors: Velican Akcakaya and Nicklas Reincke, 6th of April 2020.
